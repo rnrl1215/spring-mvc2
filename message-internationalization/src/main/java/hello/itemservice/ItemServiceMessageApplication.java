cfbc85c7fc0987edf1d28ca38b2633955caeb879
skahn <d1215@naver.com>
2022-01-08 18:12:26 +0900

    first commit

---
+++ message-internationalization/src/main/java/hello/itemservice/ItemServiceMessageApplication.java
@@ -0,0 +1,13 @@
+package hello.itemservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItemServiceMessageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItemServiceMessageApplication.class, args);
+	}
+
+}
