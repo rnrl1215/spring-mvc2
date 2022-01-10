fd5a534e283d42e52db0478dcb9cad402461b282
skahn <d1215@naver.com>
2022-01-11 01:23:25 +0900

    add messages properties for

---
+++ message-internationalization/src/test/java/hello/itemservice/message/MessageSourceTest.java
@@ -0,0 +1,22 @@
+package hello.itemservice.message;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.MessageSource;
+
+import java.util.Locale;
+
+@SpringBootTest
+public class MessageSourceTest {
+
+    @Autowired
+    MessageSource messageSource;
+
+    @Test
+    void helloMessage() {
+        String result = messageSource.getMessage("hello", null, null);
+        Assertions.assertThat(result).isEqualTo("안녕");
+    }
+}
