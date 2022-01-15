37fb6aa80410cddb988c5fc3573cdc3d07372962
skahn <d1215@naver.com>
2022-01-15 23:53:25 +0900

    feat: add validation

---
+++ validation/src/main/java/hello/itemservice/ItemServiceValidationApplication.java
@@ -0,0 +1,13 @@
+package hello.itemservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItemServiceValidationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItemServiceValidationApplication.class, args);
+	}
+
+}
