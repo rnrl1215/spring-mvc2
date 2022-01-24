f859275a41d2bec407c9d40fa579ecec71332da4
skahn <d1215@naver.com>
2022-01-25 00:18:01 +0900

    feat: add validation version2

--- validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
+++ validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
@@ -46,8 +46,8 @@ public class ValidationItemControllerV2 {
     }
 
     //@PostMapping("/add")
-    public String addItemV1(@ModelAttribute Item item, RedirectAttributes redirectAttributes,
-                          BindingResult bindingResult, Model model) {
+    public String addItemV1(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes,
+                           Model model) {
         // Binding Result 를 사용하면 erros 역할을 대신해준다.
         // 모델에 안담아도 자동으로 넘어간다.
         Map<String, String> errors = new HashMap<>();
@@ -88,8 +88,8 @@ public class ValidationItemControllerV2 {
     }
 
     //@PostMapping("/add")
-    public String addItemV2(@ModelAttribute Item item, RedirectAttributes redirectAttributes,
-                            BindingResult bindingResult, Model model) {
+    public String addItemV2(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes,
+                             Model model) {
 
         // Binding Result 를 사용하면 erros 역할을 대신해준다.
         // 모델에 안담아도 자동으로 넘어간다.
@@ -135,9 +135,9 @@ public class ValidationItemControllerV2 {
     }
 
 
-    @PostMapping("/add")
-    public String addItemV3(@ModelAttribute Item item, RedirectAttributes redirectAttributes,
-                            BindingResult bindingResult, Model model) {
+    //@PostMapping("/add")
+    public String addItemV3(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes,
+                             Model model) {
 
         // Binding Result 를 사용하면 erros 역할을 대신해준다.
         // 모델에 안담아도 자동으로 넘어간다.
@@ -165,6 +165,61 @@ public class ValidationItemControllerV2 {
             if (resultPrice < 100000) {
                 //errors.put("globalError", "가격 * 수량의 합은 10,000원 이상 이어야 합니다. 현재값 = " + resultPrice);
                 bindingResult.addError(new ObjectError("item",  new String[]{"totalPriceMin"},new Object[]{10000, resultPrice},null));
+            }
+        }
+
+        // 검증 실패시 다시 입력 폼으로
+        // if (!errors.isEmpty()) {
+        if (bindingResult.hasErrors()) {
+            log.info("errors = {}", bindingResult);
+            //model.addAttribute("errors", errors);
+            return "validation/v2/addForm";
+        }
+
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/validation/v2/items/{itemId}";
+    }
+
+    @PostMapping("/add")
+    public String addItemV4(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes,
+                            Model model) {
+
+        // Binding Result 를 사용하면 erros 역할을 대신해준다.
+        // 모델에 안담아도 자동으로 넘어간다.
+        Map<String, String> errors = new HashMap<>();
+
+        // Item 뒤에 BindingResult가 오는 것을 유의해서 보자
+        // 다음과 같이 BindingResult는 타겟을 미리 알 수 있다.
+        log.info("objectName = {}", bindingResult.getObjectName());
+        log.info("target = {}", bindingResult.getTarget());
+
+        // 검증 로직
+        if (!StringUtils.hasText(item.getItemName())) {
+            //bindingResult.addError(new FieldError("item", "itemName", item.getItemName(), false, new String[]{"required.item.itemName"}, null,  null));
+            bindingResult.rejectValue("itemName", "required");
+        }
+        if (item.getPrice() == null || item.getPrice() < 1000 || item.getPrice() > 1000000) {
+            //errors.put("price", "가격은 1,000 에서 ~ 1,000,000 까지 허용합니다.");
+            //bindingResult.addError(new FieldError("item", "price", "가격은 1,000 에서 ~ 1,000,000 까지 허용합니다."));
+            //bindingResult.addError(new FieldError("item", "price", item.getPrice(), false,  new String[]{"range.item.price"}, new Object[]{1000,1000000}, null));
+            bindingResult.rejectValue("price", "range", new Object[]{1000,1000000}, null);
+        }
+        if (item.getQuantity() == null || item.getQuantity() >= 9999) {
+            // errors.put("quantity", "수량은 최대 9,999 까지 허용합니다.");
+            //bindingResult.addError(new FieldError("item", "quantity", "수량은 최대 9,999 까지 허용합니다."));
+            //bindingResult.addError(new FieldError("item", "quantity", item.getQuantity(), false, new String[]{"max.item.quantity"}, new Object[]{9999},  null));
+            bindingResult.rejectValue("quantity", "max", new Object[]{9999},  null);
+        }
+
+        // 특정 필드가 아닌 복합 룰 검증
+        if (item.getPrice() != null && item.getQuantity() != null) {
+            int resultPrice = item.getPrice() * item.getQuantity();
+            if (resultPrice < 100000) {
+                //errors.put("globalError", "가격 * 수량의 합은 10,000원 이상 이어야 합니다. 현재값 = " + resultPrice);
+                //bindingResult.addError(new ObjectError("item",  new String[]{"totalPriceMin"},new Object[]{10000, resultPrice},null));
+                bindingResult.reject("totalPriceMin", new Object[]{10000, resultPrice},null);
             }
         }
 
