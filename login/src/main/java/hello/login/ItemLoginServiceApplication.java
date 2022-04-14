56d1999a3be38f118002c89f59c1b33682c2aa34
skahn <d1215@naver.com>
2022-04-14 23:39:06 +0900

    feat: Add SessionAttribute

---
+++ login/src/main/java/hello/login/ItemLoginServiceApplication.java
@@ -0,0 +1,13 @@
+package hello.login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItemLoginServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItemLoginServiceApplication.class, args);
+	}
+
+}
---
+++ login/src/main/java/hello/login/web/HomeController.java
@@ -0,0 +1,101 @@
+package hello.login.web;
+
+import hello.login.domain.member.Member;
+import hello.login.domain.member.MemberRepository;
+import hello.login.web.session.SessionManager;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+@Slf4j
+@Controller
+@RequiredArgsConstructor
+public class HomeController {
+
+    private final MemberRepository memberRepository;
+    private final SessionManager sessionManager;
+
+   // @GetMapping("/")
+    public String home() {
+        return "home";
+    }
+
+
+
+    @GetMapping("/")
+    // SessionAttribute 해당 name 을 가진 세션을 찾아온다. 생성을 하진 않는다.
+    public String loginHomeV3Spring(@SessionAttribute(name = SessionConst.LOGIN_MEMBER, required = false)Member member, Model model) {
+
+        if (member == null) {
+            return "home";
+        }
+
+        model.addAttribute("member", member);
+
+        return "loginHome";
+    }
+
+
+    //@GetMapping("/")
+    public String loginHomeV3(HttpServletRequest request, Model model) {
+
+        //Member member = (Member) sessionManager.getSession(request);
+
+        HttpSession session = request.getSession(false);
+
+        if (session == null) {
+            return "home";
+        }
+
+
+        Member member = (Member)session.getAttribute(SessionConst.LOGIN_MEMBER);
+
+        if (member == null) {
+            return "home";
+        }
+
+
+        model.addAttribute("member", member);
+
+        return "loginHome";
+    }
+
+
+    //@GetMapping("/")
+    public String loginHomeV2(HttpServletRequest request, Model model) {
+
+        Member member = (Member) sessionManager.getSession(request);
+
+
+        if (member == null) {
+            return "home";
+        }
+
+        model.addAttribute("member", member);
+
+        return "loginHome";
+    }
+
+    //@GetMapping("/")
+    public String loginHome(@CookieValue(name = "memberId", required = false) Long memberId, Model model) {
+        if (memberId == null) {
+            return "home";
+        }
+
+        Member loginMember = memberRepository.findById(memberId);
+        if (loginMember == null) {
+            return "home";
+        }
+
+        model.addAttribute("member", loginMember);
+
+        return "loginHome";
+    }
+}
